6f6bf0acc296bc27800c0852248274f2cc0480b7
Keyto <balvesca@gmail.com>
2024-02-01 23:25:30 +0100

    Some great changes to mùake it similar to the exercice

--- Looter/ContentView.swift
+++ Looter/ContentView.swift
@@ -7,17 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+class Inventory: ObservableObject {
+    @Published var loot = [LootItem]()
+    
+    init(loot: [LootItem]) {
+        self.loot = loot
+    }
+    
+    func addLoot(item: LootItem) {
+        loot.append(item)
     }
 }
+
+let testInventory: [LootItem] = [
+    LootItem(quantity : 1, name: "Excalibur", type: .dagger, rarity:.legendary, attackStrength: 50, game: availableGames[1]),
+    LootItem(quantity : 2,  name: "Dragon Shield", type: .shield, rarity:.epic, attackStrength: 0, game: availableGames[0]),
+    LootItem(quantity : 13, name: "Fireball Staff", type: .magic, rarity:.rare, attackStrength: 35, game: availableGames[4]),
+    LootItem(quantity : 9, name: "Poison Dagger", type: .dagger, rarity:.unique, attackStrength: 30, game: availableGames[3]),
+]
+
+struct ContentView: View {
+    @StateObject var inventory = Inventory(loot: testInventory)
+    
+    @State var showAddItemView: Bool = false
+
+    
+    var body: some View {
+        NavigationStack {
+                    List {
+                        ForEach(inventory.loot) { item in
+                            NavigationLink(destination: LootDetailView(item: item)) {
+                                VStack(alignment: .leading) {
+                                    HStack() {
+                                        Circle().frame(width: 15, height: 15)
+                                            .foregroundColor(item.rarity.color)
+                                        Text(item.name)
+                                        Spacer()
+                                        Text(item.type.rawValue)
+                                    }
+                                    Text("Quantity : \(item.quantity)")
+                                }
+                            }
+                        }
+                    }.navigationBarTitle("Inventory") // Notre titre de page, choisissez le titre que vous voulez
+                .toolbar(content: { // La barre d'outil de notre page
+                    ToolbarItem(placement: ToolbarItemPlacement.automatic) {
+                        Button(action: {
+                            showAddItemView.toggle() // L'action de notre bouton
+                        }, label: {
+                            Image(systemName: "plus.circle.fill")
+                        })
+                    }
+                })
+            .sheet(isPresented: $showAddItemView, content: {
+                AddItemView().environmentObject(inventory)
+            })
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
--- Looter/LootItem.swift
+++ Looter/LootItem.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+struct LootItem: Identifiable {
+    var id = UUID()
+    var quantity: Int = 1
+    var name: String
+    var type: ItemType
+    var rarity: Rarity
+    var attackStrength: Int?
+    var game: Game?
+}
+
+enum ItemType: String, CaseIterable {
+    case magic = "⭐️"
+    case fire = "🔥"
+    case ice = "❄️"
+    case wind = "💨"
+    case poison = "☠️"
+    case thunder = "⚡️"
+    case dagger = "🗡️"
+    case shield = "🛡️"
+    case bow = "🏹"
+    case ring = "💍"
+    case unknown = "🎲"
+}
+
+extension Rarity {
+    var capitalized: String {
+        switch self {
+        case .common: return "Common"
+        case .uncommon: return "Uncommon"
+        case .rare: return "Rare"
+        case .epic: return "Epic"
+        case .legendary: return "Legendary"
+        case .unique: return "Unique"
+        }
+    }
+}
